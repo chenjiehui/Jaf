dc1e19a250531b9f2bf7cfc2a42deed48395f22a
Jiehui Chen <599194429@qq.com>
2017-04-10 23:52:53 +0800

    通过配置加载类实现加载配置

---
+++ core/lib/conf.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jiehuichen
+ * Date: 2017/4/10
+ * Time: 下午9:28
+ */
+namespace core\lib;
+
+class conf
+{
+    static public $conf = array();
+    static public function get($name,$file)
+    {
+        /**
+         * 1 判断配置文件是否存在
+         * 2 判断配置是否存在
+         * 3 缓存配置
+         */
+
+        if(isset(self::$conf[$file])){
+            return self::$conf[$file][$name];
+        }else{
+            $path = PATH.'/core/config/'.$file.'.php';
+            if(is_file($path)){
+                $conf = include $path;
+                if(isset($conf[$name])){
+                    self::$conf[$file] = $conf;
+                    return $conf[$name];
+                }else{
+                    throw new \Exception('没有这个配置项',$name);
+                }
+            }else{
+                throw new \Exception('找不到配置文件'.$file);
+            }
+        }
+
+    }
+
+    static public function all($file)
+    {
+        if(isset(self::$conf[$file])){
+            return self::$conf[$file];
+        }else{
+            $path = PATH.'/core/config/'.$file.'.php';
+            if(is_file($path)){
+                $conf = include $path;
+                self::$conf[$file] = $conf;
+                return $conf;
+            }else{
+                throw new \Exception('找不到配置文件'.$file);
+            }
+        }
+
+    }
+
+
+}
